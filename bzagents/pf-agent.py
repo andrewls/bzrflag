3659a8db02dd4082e53536661b598ab4c3830a3c
Andrew Stephenson <andrew@ringseven.com>
2015-09-21 07:48:12 -0600

    Got a basic framework for potential fields

---
+++ bzagents/pf-agent.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python -tt
+
+# An incredibly simple agent.  All we do is find the closest enemy tank, drive
+# towards it, and shoot.  Note that if friendly fire is allowed, you will very
+# often kill your own tanks with this code.
+
+#################################################################
+# NOTE TO STUDENTS
+# This is a starting point for you.  You will need to greatly
+# modify this code if you want to do anything useful.  But this
+# should help you to know how to interact with BZRC in order to
+# get the information you need.
+#
+# After starting the bzrflag server, this is one way to start
+# this code:
+# python agent0.py [hostname] [port]
+#
+# Often this translates to something like the following (with the
+# port name being printed out by the bzrflag server):
+# python agent0.py localhost 49857
+#################################################################
+
+import sys
+import math
+import time
+
+from bzrc import BZRC, Command
+
+class Agent(object):
+    """Class handles all command and control logic for a teams tanks."""
+
+    def __init__(self, bzrc):
+        self.bzrc = bzrc
+        self.constants = self.bzrc.get_constants()
+        self.commands = []
+
+    def tick(self, time_diff):
+        """Some time has passed; decide what to do next."""
+        mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
+        self.mytanks = mytanks
+        self.othertanks = othertanks
+        self.flags = flags
+        self.shots = shots
+        self.enemies = [tank for tank in othertanks if tank.color !=
+                        self.constants['team']]
+        self.obstacles = self.bzrc.read_obstacles()
+
+        self.potential_fields = {}
+
+        self.commands = []
+
+        for tank in mytanks:
+            self.calculate_attractive_fields()
+            self.calculate_repulsive_fields()
+            self.calculate_tangential_fields()
+
+        for key in potential_fields.keys():
+            # reduce potential fields to one
+            # move in direction based off of dx and dy
+            print "Merging potential fields for tank %d" % key 
+
+        results = self.bzrc.do_commands(self.commands)
+
+    def calculate_attractive_fields(self, tank, flags):
+        for flag in flags:
+            # get flag location
+            # calculate distance between flag and tank
+            # calculate angle between flag and tank
+            # calculate dx and dy based off of
+
+    def calculate_repulsive_fields(self):
+        pass
+
+    def calculate_tangential_fields(self):
+        pass
+
+def main():
+    # Process CLI arguments.
+    try:
+        execname, host, port = sys.argv
+    except ValueError:
+        execname = sys.argv[0]
+        print >>sys.stderr, '%s: incorrect number of arguments' % execname
+        print >>sys.stderr, 'usage: %s hostname port' % sys.argv[0]
+        sys.exit(-1)
+
+    # Connect.
+    #bzrc = BZRC(host, int(port), debug=True)
+    bzrc = BZRC(host, int(port))
+
+    agent = Agent(bzrc)
+
+    prev_time = time.time()
+
+    # Run the agent
+    try:
+        while True:
+            time_diff = time.time() - prev_time
+            agent.tick(time_diff)
+    except KeyboardInterrupt:
+        print "Exiting due to keyboard interrupt."
+        bzrc.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: et sw=4 sts=4
