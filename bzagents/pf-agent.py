df2301b442ed1e6fd346ba515c5479bddc3cf5f6
Andrew Stephenson <andrew@ringseven.com>
2015-09-23 10:09:09 -0600

    Repulsive fields are kinda sorta not really working.

--- bzagents/pf-agent.py
+++ bzagents/pf-agent.py
@@ -29,6 +29,9 @@ from bzrc import BZRC, Command
 FLAGRADIUS = 2.5
 FLAGSPREAD = 100
 
+OBSTACLERADIUS = 1
+OBSTACLESPREAD = 10
+
 class Agent(object):
     """Class handles all command and control logic for a teams tanks."""
 
@@ -59,7 +62,7 @@ class Agent(object):
         for tank in mytanks:
             self.potential_fields[tank.index] = []
             self.calculate_attractive_fields(tank, flags)
-            self.calculate_repulsive_fields()
+            self.calculate_repulsive_fields(tank, self.obstacles, mytanks + othertanks)
             self.calculate_tangential_fields()
 
         for key in self.potential_fields.keys():
@@ -68,17 +71,20 @@ class Agent(object):
             self.merge_potential_fields(self.potential_fields[key], key)
 
         for tank in mytanks:
-            if tank.flag != '-':
-                self.return_to_base(tank)
-            else:
-                self.move_to_position(tank, tank.x + self.potential_fields[tank.index][0], tank.y + self.potential_fields[tank.index][1])
+            self.move_to_position(tank, tank.x + self.potential_fields[tank.index][0], tank.y + self.potential_fields[tank.index][1])
         results = self.bzrc.do_commands(self.commands)
 
     def return_to_base(self, tank):
         # get the coordinates of the center of the base
+        alpha = 0.9
         x = (self.base.corner1_x + self.base.corner2_x + self.base.corner3_x + self.base.corner4_x)/4
         y = (self.base.corner1_y + self.base.corner2_y + self.base.corner3_y + self.base.corner4_y)/4
-        self.move_to_position(tank, x, y)
+        distance = math.sqrt((x - tank.x)**2 + (y - tank.y)**2)
+        angle = math.atan2(y - tank.y, x - tank.x)
+        if distance < FLAGRADIUS + FLAGSPREAD:
+            self.potential_fields[tank.index].append((alpha * (distance - FLAGRADIUS) * math.cos(angle), alpha * (distance - FLAGRADIUS) * math.sin(angle)))
+        else:
+            self.potential_fields[tank.index].append((alpha * FLAGSPREAD * math.cos(angle), alpha * FLAGSPREAD * math.sin(angle)))
 
     def move_to_position(self, tank, target_x, target_y):
         """Set command to move to given coordinates."""
@@ -100,10 +106,14 @@ class Agent(object):
     def merge_potential_fields(self, fields, key):
         dx = 0
         dy = 0
+        print "Premerge"
+        print fields
         for field in fields:
             dx += field[0]
             dy += field[1]
         self.potential_fields[key] = (dx, dy)
+        print "Postmerger"
+        print self.potential_fields[key]
 
     def calculate_attractive_fields(self, tank, flags):
         alpha = 0.9
@@ -111,29 +121,57 @@ class Agent(object):
         closest_flag = None
         distance = float("inf")
         # find closest flag
-        for flag in flags:
-            if self.constants["team"] == flag.color:
-                continue
-            distance_to_flag = math.sqrt((flag.x - tank.x)**2 + (flag.y - tank.y)**2)
-            if distance_to_flag < distance:
-                distance = distance_to_flag
-                closest_flag = flag
-
-        # get flag location
-        # calculate distance between flag and tank
-        distance = math.sqrt((closest_flag.x - tank.x)**2 + (closest_flag.y - tank.y)**2)
-        # calculate angle between closest_flag and tank
-        angle = math.atan2(closest_flag.y - tank.y, closest_flag.x - tank.x)
-        # calculate dx and dy based off of distance and angle
-        if distance < FLAGRADIUS:
-            self.potential_fields[tank.index].append((0, 0))
-        elif distance < FLAGRADIUS + FLAGSPREAD:
-            self.potential_fields[tank.index].append((alpha * (distance - FLAGRADIUS) * math.cos(angle), alpha * (distance - FLAGRADIUS) * math.sin(angle)))
+        if tank.flag != '-':
+            self.return_to_base(tank)
         else:
-            self.potential_fields[tank.index].append((alpha * FLAGSPREAD * math.cos(angle), alpha * FLAGSPREAD * math.sin(angle)))
+            for flag in flags:
+                if self.constants["team"] == flag.color:
+                    continue
+                distance_to_flag = math.sqrt((flag.x - tank.x)**2 + (flag.y - tank.y)**2)
+                if distance_to_flag < distance:
+                    distance = distance_to_flag
+                    closest_flag = flag
 
-    def calculate_repulsive_fields(self):
-        pass
+            # get flag location
+            # calculate distance between flag and tank
+            distance = math.sqrt((closest_flag.x - tank.x)**2 + (closest_flag.y - tank.y)**2)
+            # calculate angle between closest_flag and tank
+            angle = math.atan2(closest_flag.y - tank.y, closest_flag.x - tank.x)
+            # calculate dx and dy based off of distance and angle
+            if distance < FLAGRADIUS:
+                self.potential_fields[tank.index].append((0, 0))
+            elif distance < FLAGRADIUS + FLAGSPREAD:
+                self.potential_fields[tank.index].append((alpha * (distance - FLAGRADIUS) * math.cos(angle), alpha * (distance - FLAGRADIUS) * math.sin(angle)))
+            else:
+                self.potential_fields[tank.index].append((alpha * FLAGSPREAD * math.cos(angle), alpha * FLAGSPREAD * math.sin(angle)))
+
+    def calculate_repulsive_fields(self, tank, obstacles, tanks):
+        alpha = 0.9
+        INFINITY = 1000000
+        for obstacle in obstacles:
+            for corner in obstacle:
+                distance = math.sqrt((corner[0] - tank.x)**2 + (corner[1] - tank.y)**2)
+                angle = math.atan2(corner[1] - tank.y, corner[0] - tank.x)
+                print "Tank %d is %f from obstacle" % (tank.index, distance)
+
+                if distance < OBSTACLERADIUS:
+                    self.potential_fields[tank.index].append((- math.copysign(1, math.cos(angle)) * INFINITY, -math.copysign(1, math.sin(angle)) * INFINITY))
+                elif distance < OBSTACLERADIUS + OBSTACLESPREAD:
+                    self.potential_fields[tank.index].append((-alpha * (OBSTACLESPREAD + OBSTACLERADIUS - distance) * math.cos(angle), -alpha * (OBSTACLESPREAD + OBSTACLERADIUS - distance) * math.sin(angle)))
+                print "Potential field that was calculated:"
+
+        TANKRADIUS = 2
+        TANKSPREAD = 5
+        TANKALPHA = 0.1
+        # for other_tank in tanks:
+        #     distance = math.sqrt((other_tank.x - tank.x)**2 + (other_tank.y - tank.y)**2)
+        #     angle = math.atan2(other_tank.y - tank.y, other_tank.x - tank.x)
+        #
+        #     if distance < OBSTACLERADIUS:
+        #         self.potential_fields[tank.index].append((- math.copysign(1, math.cos(angle)) * INFINITY, -math.copysign(1, math.sin(angle)) * INFINITY))
+        #     elif distance < OBSTACLERADIUS + OBSTACLESPREAD:
+        #         self.potential_fields[tank.index].append((-TANKALPHA * (OBSTACLESPREAD + OBSTACLERADIUS - distance) * math.cos(angle), -TANKALPHA * (OBSTACLESPREAD + OBSTACLERADIUS - distance) * math.sin(angle)))
+        print self.potential_fields[tank.index][-1]
 
     def calculate_tangential_fields(self):
         pass
--- run-potential-fields.sh
+++ run-potential-fields.sh
@@ -1,3 +1,7 @@
-./bin/bzrflag --world=maps/four_ls.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
+# ./bin/bzrflag --world=maps/four_ls.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
+./bin/bzrflag --world=maps/pacman.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
 python bzagents/pf-agent.py localhost 50100 &
+python bzagents/really-dumb-agent.py localhost 50101 &
+python bzagents/really-dumb-agent.py localhost 50102 &
+python bzagents/really-dumb-agent.py localhost 50103 &
